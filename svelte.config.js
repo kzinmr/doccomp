fa90a636487d5cd3082ccbd256e8b64302b8eedd
Kazuki Inamura <kzinmr109@gmail.com>
2023-08-24 08:43:05 +0900

    disable CSP-report-only

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
-    csp: {
-      directives: {
-        'script-src': ['self']
-      },
-      reportOnly: {
-        'script-src': ['self']
-      }
-    }
-  }
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	}
 };
 
 export default config;
